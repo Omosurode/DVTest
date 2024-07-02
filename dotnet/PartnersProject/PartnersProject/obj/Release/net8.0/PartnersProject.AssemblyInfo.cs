ab2a757fb6d31c623d6a058577212bc6028f7bd6
Omosurode <jacobo.gallego2@hotmail.com>
2024-07-01 23:45:39 -0500

    React app created and containerized

--- dotnet/PartnersProject/PartnersProject/obj/Release/net8.0/PartnersProject.AssemblyInfo.cs
+++ dotnet/PartnersProject/PartnersProject/obj/Release/net8.0/PartnersProject.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PartnersProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce90351f3ae83b7ec6e2157c3bf24c0493b9096b")]
 [assembly: System.Reflection.AssemblyProductAttribute("PartnersProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PartnersProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
